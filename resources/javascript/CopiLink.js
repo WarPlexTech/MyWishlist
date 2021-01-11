76ccf3a12007fa54212317f36c902d4e052a1bfe
Antoine BEZOMBES <17477476+WarPlexTech@users.noreply.github.com>
2021-01-11 14:53:50 +0100

    Ameliorations mineurs

--- resources/javascript/CopiLink.js
+++ resources/javascript/CopiLink.js
@@ -1,6 +1,13 @@
 
-function CopiLink(idlist){
+function CopiLink(idlist, sourceID){
 
     navigator.clipboard.writeText(document.location.origin+idlist);
 
+    $('.greenButton').each(function (i, element) {
+        element.classList.remove("greenButton");
+        element.classList.add("normalButton");
+    })
+
+    document.getElementById(sourceID).classList.remove("normalButton");
+    document.getElementById(sourceID).classList.add("greenButton");
 }
